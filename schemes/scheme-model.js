16e84ffa3746815c5d27cf5c8ab49e3fa0af5da1
vy3191 <venkyafhs@gmail.com>
2020-01-12 11:06:16 -0500

    Completed findSteps method

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,7 +1,6 @@
 const db = require("../data/db-config");
 
-function find() {
-  
+function find() {  
    return db("schemes").select();
 }
 
@@ -9,8 +8,12 @@ function findById(id) {
   return db("schemes").where({id}).first();
 }
 
-function findSteps() {
-  
+function findSteps(id) {  
+  return db("steps")
+         .join("schemes", "schemes.id", "steps.scheme_id")
+         .where({"steps.scheme_id":id})
+         .select("steps.id", "schemes.scheme_name", "steps.step_number","steps.instructions")
+         .orderBy("steps.step_number")
 }
 
 async function add(body) {
@@ -32,5 +35,6 @@ module.exports = {
    findById,
    add,
    remove,
-   update
+   update,
+   findSteps
 }
